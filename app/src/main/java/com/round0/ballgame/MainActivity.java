0d72ea1a767e75e64e99c2afa039fbd5256e894c
round00 <round_0@yeah.net>
2018-11-29 10:24:02 +0800

    use localbroadcast to process gameover

--- app/src/main/java/com/round0/ballgame/MainActivity.java
+++ app/src/main/java/com/round0/ballgame/MainActivity.java
@@ -1,20 +1,28 @@
 package com.round0.ballgame;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.WindowManager;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private GameOverReceiver localReceiver;
+    private LocalBroadcastManager localBroadcastManager;
 
     SensorManager mSensorManager;
     Sensor mAccelerSensor;
@@ -29,6 +37,13 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         super.onCreate(savedInstanceState);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        //初始化和注册本地广播
+        localBroadcastManager = LocalBroadcastManager.getInstance(this);
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction("com.round0.ballgame.GAMEOVER_BROADCAST");
+        localReceiver = new GameOverReceiver();
+        localBroadcastManager.registerReceiver(localReceiver, intentFilter);
 
         //传感器初始化
         mSensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
@@ -56,31 +71,10 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         float[] values = event.values;
         mGameManager.updateBall(values[1],values[0]);
         int result = mGameManager.check();
-
-        //这里的处理不太好， 可以考虑改成本地广播的方式实现
-        if(result == 2){//碰到障碍物，死了
-            mSensorManager.unregisterListener(this);
-            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-            builder.setTitle("提示");
-            builder.setMessage("你挂了");
-            builder.setCancelable(false);
-            builder.setPositiveButton("在来一次", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    mGameManager.init();
-                    mSensorManager.registerListener(MainActivity.this, mAccelerSensor,
-                            SensorManager.SENSOR_DELAY_GAME);
-                }
-            });
-            builder.setNegativeButton("不来了", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    finish();
-                }
-            });
-            builder.show();
+        if(result == 2) {//碰到障碍物，死了
+            Intent intent = new Intent("com.round0.ballgame.GAMEOVER_BROADCAST");
+            localBroadcastManager.sendBroadcast(intent);
         }
-
     }
 
     @Override
@@ -99,5 +93,37 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     protected void onPause() {
         super.onPause();
         mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        localBroadcastManager.unregisterReceiver(localReceiver);
+    }
+
+    class GameOverReceiver extends BroadcastReceiver{
+        @Override
+        public void onReceive(Context context, Intent intent) {
+                mSensorManager.unregisterListener(MainActivity.this);
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("提示");
+                builder.setMessage("你挂了");
+                builder.setCancelable(false);
+                builder.setPositiveButton("在来一次", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        mGameManager.init();
+                        mSensorManager.registerListener(MainActivity.this, mAccelerSensor,
+                                SensorManager.SENSOR_DELAY_GAME);
+                    }
+                });
+                builder.setNegativeButton("不来了", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                });
+                builder.show();
+        }
     }
 }
